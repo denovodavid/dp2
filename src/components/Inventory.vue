64c3586bb6a869e5597344b15b2e9af8e0b4814b
Alex Nirenovic <a.nirenovic@gmail.com>
2018-04-08 23:43:39 +1000

    Updates to Inventory + added components

--- src/components/Inventory.vue
+++ src/components/Inventory.vue
@@ -1,53 +1,85 @@
 <template>
   <div>
-    <h1>This is Inventory Page!</h1>
-    <span>Name: </span><input type="text" v-model.trim="newItem.name"><br>
-    <span>Category: </span><input type="text" v-model.trim="newItem.category"><br>
-    <span>Price: </span><input type="number" v-model="newItem.price"><br>
-    <span>Quantity: </span><input type="number" v-model="newItem.quantity"><br>
-    <button @click="addInventoryItem">Add To Inventory</button>
-    <br><br>
-    <button
-      @click="editInventory = !editInventory"
-    >
-      {{ editInventory ? 'Done' : 'Edit' }}
-    </button>
-    <table border="1">
-      <thead>
-        <tr>
-          <th>Product</th>
-          <th>Category</th>
-          <th>Price</th>
-          <th>Quantity</th>
-          <th v-show="editInventory">Remove</th>
-        </tr>
-      </thead>
-      <tbody v-show="!editInventory">
-        <tr
-          v-for="item in $root.$data.inventory"
-          :key="item.id"
+    <div class="banner">
+      <div class="inner">
+        <div class="banner-title">
+          <span>Inventory</span>
+        </div>
+      </div>
+    </div>
+    <div class="content-block centered">
+      <div class="inner">
+        <div class="input-block">
+          <div class="input-column">
+            <label>Name</label>
+            <input type="text" v-model.trim="newItem.name"><br>
+          </div>
+        </div>
+        <div class="input-block">
+          <div class="input-column">
+            <label>Category</label>
+            <input type="text" v-model.trim="newItem.category"><br>
+          </div>
+        </div>
+        <div class="input-block">
+          <div class="input-column half">
+            <label>Price</label>
+            <input type="number" v-model="newItem.price"><br>
+          </div>
+          <div class="input-column half">
+            <label>Quantity</label>
+            <input type="number" v-model="newItem.quantity"><br>
+          </div>
+        </div>
+        <div class="input-block">
+          <div class="input-column">
+            <button @click="addInventoryItem">Add To Inventory</button>
+          </div>
+        </div>
+        <br><br>
+        <button
+          @click="editInventory = !editInventory"
         >
-          <td>{{ item.name }}</td>
-          <td>{{ item.category }}</td>
-          <td>{{ item.price }}</td>
-          <td>{{ item.quantity }}</td>
-        </tr>
-      </tbody>
-      <tbody v-show="editInventory">
-        <tr
-          v-for="item in $root.$data.inventory"
-          :key="item.id"
-        >
-          <td><input type="text" v-model="item.name"></td>
-          <td><input type="text" v-model="item.category"></td>
-          <td><input type="number" v-model="item.price"></td>
-          <td><input type="number" v-model="item.quantity"></td>
-          <td v-show="editInventory">
-            <button @click="$root.removeInventoryItem(item.id)">Remove</button>
-          </td>
-        </tr>
-      </tbody>
-    </table>
+          {{ editInventory ? 'Done' : 'Edit' }}
+        </button>
+        <table border="1">
+          <thead>
+            <tr>
+              <th>Product</th>
+              <th>Category</th>
+              <th>Price</th>
+              <th>Quantity</th>
+              <th v-show="editInventory">Remove</th>
+            </tr>
+          </thead>
+          <tbody v-show="!editInventory">
+            <tr
+              v-for="item in $root.$data.inventory"
+              :key="item.id"
+            >
+              <td>{{ item.name }}</td>
+              <td>{{ item.category }}</td>
+              <td>{{ item.price }}</td>
+              <td>{{ item.quantity }}</td>
+            </tr>
+          </tbody>
+          <tbody v-show="editInventory">
+            <tr
+              v-for="item in $root.$data.inventory"
+              :key="item.id"
+            >
+              <td><input type="text" v-model="item.name"></td>
+              <td><input type="text" v-model="item.category"></td>
+              <td><input type="number" v-model="item.price"></td>
+              <td><input type="number" v-model="item.quantity"></td>
+              <td v-show="editInventory">
+                <button @click="$root.removeInventoryItem(item.id)">Remove</button>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
   </div>
 </template>
 
