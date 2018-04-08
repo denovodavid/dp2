2cd55fae5c6e43e39520dc70f1aeed1ee31de5d8
100515973 <100515973@student.swin.edu.au>
2018-04-08 19:58:31 +1000

    Added Sales Page

--- src/components/Sales.vue
+++ src/components/Sales.vue
@@ -1,12 +1,100 @@
 <template>
   <div>
     <h1>This is Sales page!</h1>
+    <span>Transaction Number: </span><input type="number" v-model.trim="newRecord.transactionNumber"><br>
+    <span>Transaction Date: </span><input type="text" v-model.trim="newRecord.transDate"><br>
+    <span>Inventory id: </span><input type="text" v-model="newRecord.name"><br>
+    <span>Quantity: </span><input type="number" v-model="newRecord.quantity"><br>
+    <span>Price: </span><input type="number" v-model="newRecord.priceEach"><br>
+    <span>Total: </span><input type="number" v-model="newRecord.total"><br>
+    <button @click="addSalesRecord">Add To Sales</button>
+    <br><br>
+    <button
+      @click="editSales = !editSales"
+    >
+      {{ editSales ? 'Done' : 'Edit' }}
+    </button>
+        <table border="1">
+      <thead>
+        <tr>
+          <th>Transaction Number</th>
+          <th>Transaction Date</th>
+          <th>Inventory id</th>
+          <th>Quantity</th>
+          <th>Price</th>
+          <th>Total</th>
+          <th v-show="editSales">Remove</th>
+        </tr>
+      </thead>
+      <tbody v-show="!editSales">
+        <tr
+          v-for="record in $root.$data.sales"
+          :key="record.id"
+        >
+          <td>{{ record.transactionNumber }}</td>             <!--TODO: auto generate-->
+          <td>{{ record.transactionDate }}</td>               <!--TODO: date picker-->
+          <td>{{ record.id }}</td>                            <!--TODO: intergrate with inventory id-->
+          <td>{{ record.quantity }}</td>                      <!--TODO: change inventory items quantity-->
+          <td>{{ record.priceEach}}</td>
+          <td>{{ record.total }}</td>
+        </tr>
+      </tbody>
+      <tbody v-show="editSales">
+        <tr
+          v-for="record in $root.$data.sales"
+          :key="record.id"
+        >
+          <td><input type="number" v-model="record.transactionNumber"></td>
+          <td><input type="text" v-model="record.transactionDate"></td>
+          <td><input type="text" v-model="record.id"></td>
+          <td><input type="number" v-model="record.quantity"></td>
+          <td><input type="number" v-model="record.priceEach"></td>
+          <td><input type="number" v-model="record.total"></td>
+          <td v-show="editSales">
+            <button @click="$root.removeSalesRecord(record.id)">Remove</button>
+          </td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'Sales'
+  name: 'Sales',
+  data () {
+    return {
+      newRecord: {
+        transactionNumber: 0,
+        transactionDate: '',
+        id: '',
+        quantity: 1,
+        priceEach: 0,
+        total: 0
+      },
+      editSales: false
+    }
+  },
+  methods: {
+    addSalesRecord () {
+      const record = {
+        transactionNumber: this.newRecord.transactionNumber,
+        transactionDate: this.newRecord.transactionDate,
+        price: this.newRecord.id,
+        quantity: this.newRecord.quantity,
+        priceEach: this.newRecord.priceEach,
+        total: this.newRecord.total
+
+      }
+      this.$root.addSalesRecord(record)
+      this.newRecord.transationNumber = 0
+      this.newRecord.transactionDate = ''
+      this.newRecord.id = ''
+      this.newRecord.quantity = 1
+      this.newRecord.priceEach = 0
+      this.newRecord.total = 0
+    }
+  }
 }
 </script>
 
--- src/sales.js
+++ src/sales.js
@@ -4,26 +4,43 @@ export default {
   data: {
     sales: [
       {
-        products: [
-          {
-            id: '5ccdd07c-b5c2-417c-a2ef-bcbe422d06c2',
-            quantity: 1,
-            priceEach: 5.99
-          },
-          {
-            id: '403d687a-e6df-4e74-b4f8-5f6ac42c130f',
-            quantity: 3,
-            priceEach: 11.99
-          }
-        ],
-        id: 'da2db1d8-6397-4012-8e39-104883ab151a'
+        transactionNumber: 1,
+        transactionDate: '2018-03-29',
+        id: '5ccdd07c-b5c2-417c-a2ef-bcbe422d06c2',
+        quantity: 1,
+        priceEach: 5.99,
+        total: 1 * 5.99
+      },
+      {
+        transactionNumber: 2,
+        transactionDate: '2018-04-03',
+        id: '403d687a-e6df-4e74-b4f8-5f6ac42c130f',
+        quantity: 3,
+        priceEach: 11.99,
+        total: 3 * 11.99
       }
     ]
   },
   methods: {
     addSalesRecord (record) {
-      record.id = uuidv4()
-      this.sales.push(record)
+      if (record.transactionNumber !== '' &&
+      record.transactionDate !== '' &&
+      record.id !== '' &&
+      record.quantity > 0 &&
+      record.priceEach > 0 &&
+      record.total > 0
+      ) {
+        record.id = uuidv4()
+        this.sales.push(record)
+        return record.id
+      }
+      return null
+    },
+    removeSalesRecord (id) {
+      const record = this.sales.find(record => record.id === id)
+      if (record == null) return
+      const index = this.sales.indexOf(record)
+      this.sales.splice(index, 1)
     }
   }
 }
