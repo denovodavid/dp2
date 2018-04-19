e13bf8b318a96f3910f0dc426137c03f4bfee953
smeagolem <stooge213@hotmail.com>
2018-04-19 11:03:26 +1000

    :sparkles: add start of monthly predictions

--- src/components/Reports.vue
+++ src/components/Reports.vue
@@ -1,8 +1,48 @@
 <template>
-  <div class="banner">
-    <div class="inner">
-      <div class="banner-title">
-        <span>Reports</span>
+  <div id="sales" class="page">
+    <div class="content-block centered">
+      <div class="inner">
+        <div class="form-container">
+          <h4>Item</h4>
+          <label for="item">Select an item:</label>
+          <select
+            name="item"
+            v-model="selectedItem"
+          >
+            <option
+              v-for="item in $root.inventory"
+              :key="item.id"
+              :value="item"
+            >
+              {{ item.name }}
+            </option>
+          </select>
+          <br><br>
+          <p>Average Monthly Sales: {{ averageMonthlyItemSales }}</p>
+          <p>Previous Month Sales: {{ lastMonthItemSales }}</p>
+          <h2>Predicted Monthly Sales: {{ predictedMonthlyItemSales }}</h2>
+        </div>
+        <hr>
+        <div class="form-container">
+          <h4>Category</h4>
+          <label for="item">Select a category:</label>
+          <select
+            name="item"
+            v-model="selectedCategory"
+          >
+            <option
+              v-for="category in categories"
+              :key="category"
+              :value="category"
+            >
+              {{ category }}
+            </option>
+          </select>
+          <br><br>
+          <p>Average Monthly Sales: {{ averageMonthlyCategorySales }}</p>
+          <p>Previous Month Sales: {{ lastMonthCategorySales }}</p>
+          <h2>Predicted Monthly Sales: {{ predictedMonthlyCategorySales }}</h2>
+        </div>
       </div>
     </div>
   </div>
@@ -10,7 +50,47 @@
 
 <script>
 export default {
-  name: 'Reports'
+  name: 'reports',
+  data () {
+    return {
+      selectedItem: {},
+      selectedCategory: ''
+    }
+  },
+  computed: {
+    categories () {
+      // get all categories
+      return [...new Set(this.$root.inventory.map(item => item.category))]
+    },
+    averageMonthlyItemSales () {
+      // get all sales of item by month
+      // average them
+      return 20
+    },
+    lastMonthItemSales () {
+      // get all sales of item from last month
+      return 30
+    },
+    predictedMonthlyItemSales () {
+      // Regression towards the mean
+      return this.lastMonthItemSales +
+        (this.averageMonthlyItemSales - this.lastMonthItemSales) / 2
+    },
+    averageMonthlyCategorySales () {
+      // get all sales of category by month
+      // average them
+      return 50
+    },
+    lastMonthCategorySales () {
+      // get all sales of a category from last month
+      return 37
+    },
+    predictedMonthlyCategorySales () {
+      // Regression towards the mean
+      return this.lastMonthCategorySales +
+        (this.averageMonthlyCategorySales - this.lastMonthCategorySales) / 2
+    }
+  }
 }
 </script>
 
