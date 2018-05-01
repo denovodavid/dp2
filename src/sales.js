249c25e31494aaa5de1de9a989b850ae1c396bc1
David J Holland <stooge213@hotmail.com>
2018-05-01 13:59:58 +1000

    :speech_balloon: add more dummy sales

--- src/sales.js
+++ src/sales.js
@@ -15,6 +15,62 @@ export default {
         id: '403d687a-e6df-4e74-b4f8-5f6ac42c130f',
         quantity: 3,
         priceEach: 11.99
+      },
+      {
+        transactionNumber: 3,
+        transactionDate: '2018-05-01',
+        id: '5ccdd07c-b5c2-417c-a2ef-bcbe422d06c2',
+        quantity: 2,
+        priceEach: 5.99
+      },
+      {
+        transactionNumber: 4,
+        transactionDate: '2018-04-30',
+        id: '5ccdd07c-b5c2-417c-a2ef-bcbe422d06c2',
+        quantity: 2,
+        priceEach: 7
+      },
+      {
+        transactionNumber: 5,
+        transactionDate: '2018-03-02',
+        id: '5ccdd07c-b5c2-417c-a2ef-bcbe422d06c2',
+        quantity: 2,
+        priceEach: 5.99
+      },
+      {
+        transactionNumber: 6,
+        transactionDate: '2018-01-13',
+        id: '5ccdd07c-b5c2-417c-a2ef-bcbe422d06c2',
+        quantity: 1,
+        priceEach: 5.99
+      },
+      {
+        transactionNumber: 7,
+        transactionDate: '2018-01-12',
+        id: '5ccdd07c-b5c2-417c-a2ef-bcbe422d06c2',
+        quantity: 1,
+        priceEach: 4.50
+      },
+      {
+        transactionNumber: 8,
+        transactionDate: '2018-02-09',
+        id: '5ccdd07c-b5c2-417c-a2ef-bcbe422d06c2',
+        quantity: 1,
+        priceEach: 5.99
+      },
+      {
+        transactionNumber: 9,
+        transactionDate: '2018-02-18',
+        id: '5ccdd07c-b5c2-417c-a2ef-bcbe422d06c2',
+        quantity: 2,
+        priceEach: 5.99
+      },
+      {
+        transactionNumber: 10,
+        transactionDate: '2018-03-26',
+        id: '5ccdd07c-b5c2-417c-a2ef-bcbe422d06c2',
+        quantity: 3,
+        priceEach: 4
       }
     ]
   },
