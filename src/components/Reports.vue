ebfe92c7e7cfd88387d1096af43fc9e672493315
smeagolem <stooge213@hotmail.com>
2018-05-14 13:13:54 +1000

    :art: improve structure of reports page

--- src/components/Reports.vue
+++ src/components/Reports.vue
@@ -1,9 +1,9 @@
 <template>
-  <div id="sales" class="page">
+  <div id="reports" class="page">
     <div class="content-block centered">
       <div class="inner">
+        <h1>Item</h1>
         <div class="form-container">
-          <h4>Item</h4>
           <label for="item">Select an item:</label>
           <div class="select-container">
             <select
@@ -20,23 +20,65 @@
             </select>
           </div>
           <br>
-          <p>Average Monthly Sales: {{ averageMonthlyItemSales | money }}</p>
+          <p>Average Monthly Sales: {{ averageMonthlyItemSales | money }}<br><small>(over the last 12 months)</small></p>
           <p>Previous Month Sales: {{ lastMonthItemSales | money }}</p>
-          <h2>Predicted Monthly Sales: {{ predictedMonthlyItemSales | money }}</h2>
-          <line-chart
-            :chart-data="monthlySalesChartData"
-            :options="monthlySalesChartOptions"
-          />
-          <a
-            :href="monthlySalesCsvDataUri"
-            download="monthlySales.csv"
-          >
-            Download CSV
-          </a>
+          <p><b>Predicted Monthly Sales: {{ predictedMonthlyItemSales | money }}</b></p>
+        </div>
+        <line-chart
+          :chart-data="monthlyItemSalesChartData"
+          :options="monthlyItemSalesChartOptions"
+          :height="100"
+        />
+        <a
+          :href="monthlyItemSalesCsvDataUri"
+          download="monthlySales.csv"
+        >
+          Download CSV
+        </a>
+        <br><br>
+        <div class="form-container">
+          <h4>Transactions</h4>
+          <Label>Please select start date:</Label>
+          <div style="position: relative">
+            <date-picker v-model="monthStartDate" :config="date_config"></date-picker>
+          </div>
+          <Label>Please select end date:</Label>
+          <div style="position: relative">
+            <date-picker v-model="monthEndDate" :config="date_config"></date-picker>
+          </div>
+        </div>
+        <br>
+        <div class="table-container">
+          <table border="1">
+            <thead>
+              <tr>
+                <th>Transaction Date</th>
+                <th>Quantity</th>
+                <th>Price</th>
+                <th>Total</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr v-for="sale in getSales" :key="sale.transactionNumber">
+                <td>{{sale.transactionDate}}</td>
+                <td>{{sale.quantity}}</td>
+                <td>{{sale.priceEach | money}}</td>
+                <td>{{sale.quantity * sale.priceEach | money}}</td>
+              </tr>
+            </tbody>
+            <tfoot>
+              <tr>
+                <th>Total</th>
+                <th>{{ totalQuantity }}</th>
+                <th></th>
+                <th>{{ totalSales | money }}</th>
+              </tr>
+            </tfoot>
+          </table>
         </div>
         <hr>
+        <h1>Category</h1>
         <div class="form-container">
-          <h4>Category</h4>
           <label for="item">Select a category:</label>
           <div class="select-container">
             <select
@@ -53,84 +95,11 @@
             </select>
           </div>
           <br>
-          <p>Average Monthly Sales: {{ averageMonthlyCategorySales | money }}</p>
+          <p>Average Monthly Sales: {{ averageMonthlyCategorySales | money }}<br><small>(over the last 12 months)</small></p>
           <p>Previous Month Sales: {{ lastMonthCategorySales | money }}</p>
           <h2>Predicted Monthly Sales: {{ predictedMonthlyCategorySales | money }}</h2>
         </div>
         <hr>
-        <div class="form-container">
-          <h4>Total Item Sales by Date</h4>
-          <label for="item">Select An Item:</label>
-          <div class="select-container">
-            <select name="item" v-model="selectedItem">
-              <option v-for="item in $root.inventory" :key="item.id" :value="item">
-                {{item.name}}
-              </option>
-            </select>
-          </div>
-          <div>
-            <Label>Please select start date:</Label>
-            <div style="position: relative">
-              <date-picker v-model="monthStartDate" :config="date_config"></date-picker>
-            </div>
-          </div>
-          <div>
-            <Label>Please select end date:</Label>
-            <div style="position: relative">
-              <date-picker v-model="monthEndDate" :config="date_config"></date-picker>
-            </div>
-          </div>
-        </div>
-        <hr>
-        <h4>Total Item Sales</h4>
-        <div class="table-container">
-          <table border="1">
-            <thead>
-              <tr>
-                <th>Name</th>
-                <th>Category</th>
-                <th>Quantity</th>
-                <th>Price</th>
-                <th>Total Sales</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td>{{selectedItem.name}}</td>
-                <td>{{selectedItem.category}}</td>
-                <td>{{totalQuantity}}</td>
-                <td>{{selectedItem.price | money}}</td>
-                <td>{{totalSales | money}}</td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
-        <hr>
-        <h4>Transactions in period</h4>
-        <div class="table-container">
-          <table border="1">
-            <thead>
-              <tr>
-                <th>Transaction Number</th>
-                <th>Transaction Date</th>
-                <th>Inventory Item</th>
-                <th>Quantity</th>
-                <th>Price</th>
-                <th>Total</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr v-for="sale in getSales" :key="sale.transactionNumber">
-                <td>{{sale.transactionNumber}}</td>
-                <td>{{sale.transactionDate}}</td>
-                <td>{{selectedItem.name}}</td>
-                <td>{{sale.quantity}}</td>
-                <td>{{sale.priceEach | money}}</td>
-                <td>{{sale.quantity * sale.priceEach | money}}</td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
       </div>
     </div>
   </div>
@@ -203,6 +172,47 @@ export default {
       return this.lastMonthItemSales +
         (this.averageMonthlyItemSales - this.lastMonthItemSales) / 2
     },
+    monthlyItemSalesChartData () {
+      const months = []
+      for (let i = 0; i < 12; i++) {
+        months[11 - i] = df.subMonths(endDate, i).toISOString().slice(0, 7)
+      }
+      return {
+        labels: months,
+        datasets: [{
+          label: this.selectedItem.name,
+          data: Object.entries(this.monthlyItemSales)
+            .map(([month, sales]) => {
+              return {
+                x: new Date(month),
+                y: sumBy(sales, sale => sale.quantity * sale.priceEach)
+              }
+            })
+            .sort((a, b) => {
+              return df.isAfter(a.x, b.x)
+            })
+        }]
+      }
+    },
+    monthlyItemSalesChartOptions () {
+      return {
+        scales: {
+          xAxes: [{
+            type: 'time',
+            time: {
+              unit: 'month'
+            }
+          }]
+        }
+      }
+    },
+    monthlyItemSalesCsvDataUri () {
+      const data = this.monthlyItemSalesChartData.datasets[0].data
+      const headingLine = data.map(point => point.x).join(',')
+      const dataLine = data.map(point => point.y).join(',')
+      const csvString = [headingLine, dataLine].join('\n')
+      return 'data:text/csv;charset=utf-8,' + encodeURIComponent(csvString)
+    },
     categorySales () {
       return this.$root.sales.filter(sale => {
         const item = this.$root.inventory.find(item => item.id === sale.id)
@@ -234,47 +244,6 @@ export default {
       // Regression towards the mean
       return this.lastMonthCategorySales +
         (this.averageMonthlyCategorySales - this.lastMonthCategorySales) / 2
-    },
-    monthlySalesChartData () {
-      const months = []
-      for (let i = 0; i < 12; i++) {
-        months[11 - i] = df.subMonths(endDate, i).toISOString().slice(0, 7)
-      }
-      return {
-        labels: months,
-        datasets: [{
-          label: this.selectedItem.name,
-          data: Object.entries(this.monthlyItemSales)
-            .map(([month, sales]) => {
-              return {
-                x: new Date(month),
-                y: sumBy(sales, sale => sale.quantity * sale.priceEach)
-              }
-            })
-            .sort((a, b) => {
-              return df.isAfter(a.x, b.x)
-            })
-        }]
-      }
-    },
-    monthlySalesChartOptions () {
-      return {
-        scales: {
-          xAxes: [{
-            type: 'time',
-            time: {
-              unit: 'month'
-            }
-          }]
-        }
-      }
-    },
-    monthlySalesCsvDataUri () {
-      const data = this.monthlySalesChartData.datasets[0].data
-      const headingLine = data.map(point => point.x).join(',')
-      const dataLine = data.map(point => point.y).join(',')
-      const csvString = [headingLine, dataLine].join('\n')
-      return 'data:text/csv;charset=utf-8,' + encodeURIComponent(csvString)
     },
     getSales () {
       return this.$root.sales.filter(sale => {
